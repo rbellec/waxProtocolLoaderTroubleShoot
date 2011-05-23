ad6652cb6c6e8e1452c361e4f5029f3e712847fb
Raphaël Bellec <raphael.bellec@gmail.com>
2011-05-23 12:00:29 +0200

    Initial Commit

---
+++ waxProtocolLoaderTroubleShootTests/waxProtocolLoaderTroubleShootTests.h
@@ -0,0 +1,17 @@
+//
+//  waxProtocolLoaderTroubleShootTests.h
+//  waxProtocolLoaderTroubleShootTests
+//
+//  Created by Raphaël Bellec on 23/05/11.
+//  Copyright 2011 __MyCompanyName__. All rights reserved.
+//
+
+#import <SenTestingKit/SenTestingKit.h>
+
+
+@interface waxProtocolLoaderTroubleShootTests : SenTestCase {
+@private
+    
+}
+
+@end
---
+++ waxProtocolLoaderTroubleShootTests/waxProtocolLoaderTroubleShootTests.m
@@ -0,0 +1,33 @@
+//
+//  waxProtocolLoaderTroubleShootTests.m
+//  waxProtocolLoaderTroubleShootTests
+//
+//  Created by Raphaël Bellec on 23/05/11.
+//  Copyright 2011 __MyCompanyName__. All rights reserved.
+//
+
+#import "waxProtocolLoaderTroubleShootTests.h"
+
+
+@implementation waxProtocolLoaderTroubleShootTests
+
+- (void)setUp
+{
+    [super setUp];
+    
+    // Set-up code here.
+}
+
+- (void)tearDown
+{
+    // Tear-down code here.
+    
+    [super tearDown];
+}
+
+- (void)testExample
+{
+    STFail(@"Unit tests are not implemented yet in waxProtocolLoaderTroubleShootTests");
+}
+
+@end
