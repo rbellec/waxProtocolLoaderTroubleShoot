76768924c12a1317b99745271c8279e3c863f933
Raphaël Bellec <raphael.bellec@gmail.com>
2011-05-23 12:12:45 +0200

    Adding a Wax class using UINavigationControllerDelegate and not working.
    wax returns :
    Error opening wax scripts: ...mine/wax/tools/Framework/wax/lib/stdlib/waxClass.lua:13: Could not find protocol named 'UINavigationControllerDelegate'
    Hint: Sometimes the runtime cannot automatically find a protocol. Try adding it (via xCode) to the file ProtocolLoader.h

--- init.lua
+++ init.lua
@@ -1,2 +1,4 @@
 puts("LUA IS RUNNING")
 puts("Here is Lua talking to ObjC %s", tostring(UIApplication:sharedApplication()))
+
+require "navigationDelegate"
---
+++ navigationDelegate.lua
@@ -0,0 +1,7 @@
+
+waxClass{"navigationDelegate", NSObject, protocols={"UITableViewDataSource", "UITableViewDelegate", "UINavigationControllerDelegate"}}
+
+function init(self)
+    self.super:init()
+    return self
+end
